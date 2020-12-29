ba5f1e63cfa86bf6bfc14dee35b93057c75d5487
Sahil Patel <pci135@server7.com>
2020-12-29 14:55:59 +0530

    manage login and update screen as per new version

--- autoupdater/autoupdater_frontend.js
+++ autoupdater/autoupdater_frontend.js
@@ -9,6 +9,11 @@
        version.innerText = 'Version ' + arg.version;
    });
 
+   const loginCard = document.getElementById('login-card');
+   const username = document.getElementById('username');
+   const password = document.getElementById('password');
+   const radioButton = document.getElementsByName('urlOption');
+   const loginButton = document.getElementById('login-button');
    const notification = document.getElementById('notification');
    const message = document.getElementById('message');
    const restartButton = document.getElementById('restart-button');
@@ -16,17 +21,38 @@
        ipcRenderer.removeAllListeners('update_available');
        message.innerText = 'A new update is available. Downloading now...';
        notification.classList.remove('hidden');
+       loginCard.classList.add('login-card-disable');
+       username.disabled = true;
+       password.disabled = true;
+       radioButton.forEach(ele => {
+           ele.disabled = true;
+       });
+       loginButton.disabled = true;
    });
    ipcRenderer.on('update_downloaded', () => {
        ipcRenderer.removeAllListeners('update_downloaded');
        message.innerText = 'Update Downloaded. It will be installed on restart. Restart now?';
        restartButton.classList.remove('hidden');
        notification.classList.remove('hidden');
+       loginCard.classList.add('login-card-disable');
+       username.disabled = true;
+       password.disabled = true;
+       radioButton.forEach(ele => {
+           ele.disabled = true;
+       });
+       loginButton.disabled = true;
    });
 
-   function closeNotification() {
-       notification.classList.add('hidden');
-   }
+//    function closeNotification() {
+//        notification.classList.add('hidden');
+//        loginCard.classList.remove('login-card-disable');
+//        username.disabled = false;
+//        password.disabled = false;
+//        radioButton.forEach(ele => {
+//            ele.disabled = false;
+//        });
+//        loginButton.disabled = false;
+//    }
 
    function restartApp() {
        ipcRenderer.send('restart_app');
